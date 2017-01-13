f7a0bfb7424a3f1bf51bf902af213cb7374409b4
Ruslan Rozhkov <bladteam@gmail.com>
2017-01-13 22:22:34 +0200

    finish touches

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id         :integer          not null, primary key
+#  place_id   :integer
+#  user_id    :integer
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  score      :integer          default("0")
+#
+
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
