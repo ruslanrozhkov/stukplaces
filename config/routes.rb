b35dbc3634be33d08482af756d1a7491f8a91fa1
Ruslan Rozhkov <bladteam@gmail.com>
2017-01-13 21:00:07 +0200

    Created a dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :places
   resources :reviews
+  get 'dashboard' => 'pages#dashboard'
   devise_for :users, controllers: { registrations: 'registrations' }
   root 'pages#home'
 
