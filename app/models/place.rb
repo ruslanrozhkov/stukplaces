2c6ac8bc7acf6f556743a25a1bde86b196aa73ff
Ruslan Rozhkov <bladteam@gmail.com>
2017-01-13 16:40:26 +0200

    Added reviews to places

--- app/models/place.rb
+++ app/models/place.rb
@@ -21,4 +21,6 @@ class Place < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode
+
+  has_many :reviews, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,5 +24,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates_presence_of :name
-  has_many :places
+  has_many :places, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 end
